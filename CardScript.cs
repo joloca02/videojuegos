f1dd39b28408885fa055c48877ce68cd9d70d26c
DESKTOP-LALVV45\chema <joloca02@floridauniversitaria.es>
2021-12-08 15:00:12 +0100

    Sesión 2, ejercicio 1

---
+++ CardScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+    public Sprite imagen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = imagen;
+        transform.position = new Vector3(-1, 2, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
